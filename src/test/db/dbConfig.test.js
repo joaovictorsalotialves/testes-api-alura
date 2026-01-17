76cefe9e2b94e431a27d9d78d10319bf6beaaffe
João Victor <joaovictorsalotialves12@gmail.com>
2026-01-17 15:57:14 -0300

    Testando a conexão com o de banco de dados

---
+++ src/test/db/dbConfig.test.js
@@ -0,0 +1,23 @@
+import { describe, expect } from '@jest/globals'
+import db from '../../db/dbconfig.js'
+
+describe('Testando configDB', () => {
+  it('Teste de conexão com o banco de dados', async () => {
+    const autorMock = {
+      nome: 'Teste de conexão',
+      nacionalidade: 'BR',
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    }
+
+    const autorSalvo = await db('autores')
+      .insert(autorMock)
+      .then(retorno =>
+        db('autores')
+          .where('id', retorno[0])
+          .then(usuarioSelecionado => usuarioSelecionado[0])
+      )
+
+    expect(autorSalvo.nome).toBe(autorMock.nome)
+  })
+})
