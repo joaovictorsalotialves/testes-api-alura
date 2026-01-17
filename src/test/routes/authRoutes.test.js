71e48805450db0fceb1fafacd66626a85e489e83
João Victor <joaovictorsalotialves12@gmail.com>
2026-01-17 15:37:58 -0300

    Desafio: implementando outros testes integração

--- src/test/routes/authRoutes.test.js
+++ src/test/routes/authRoutes.test.js
@@ -20,4 +20,44 @@ describe('Testando a rota login', () => {
 
     await request(servidor).post('/login').send(loginMock).expect(500).expect('"A senha de usuario é obrigatório."')
   })
+
+  it('O login deve validar se o usuario esta cadastrado', async () => {
+    const loginMock = {
+      email: 'raphael.teste@teste.com.br',
+      senha: '123456',
+    }
+
+    await request(app)
+      .post('/login')
+      .set('Accept', 'application/json')
+      .send(loginMock)
+      .expect(500)
+      .expect('"Usuario não cadastrado."')
+  })
+
+  it('O login deve validar email e senha incorreto', async () => {
+    const loginMock = {
+      email: 'raphael@teste.com.br',
+      senha: '12345',
+    }
+
+    await request(app)
+      .post('/login')
+      .set('Accept', 'application/json')
+      .send(loginMock)
+      .expect(500)
+      .expect('"Usuario ou senha invalido."')
+  })
+
+  it('O login deve validar se esta sendo retornado um accessToken', async () => {
+    const loginMock = {
+      email: 'raphael@teste.com.br',
+      senha: '123456',
+    }
+
+    const resposta = await request(app).post('/login').set('Accept', 'application/json').send(loginMock).expect(201)
+
+    expect(resposta.body.message).toBe('Usuario conectado')
+    expect(resposta.body).toHaveProperty('accessToken')
+  })
 })
